0c393320a843e3b7adca7725f624e2b5a01b4abc
RicardoGG99 <36855481+RicardoGG99@users.noreply.github.com>
2021-03-03 10:57:46 -0400

    Add files via upload

---
+++ src/controllers/UserManager.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import java.sql.Connection;
+
+import helpers.Conexion;
+import helpers.PasswordHashing;
+
+
+public class UserManager {
+    Conexion conn = new Conexion();
+    Connection connection = conn.getConnection();
+    PasswordHashing ph = new PasswordHashing();
+    
+    //Register
+    public String register(String cedula, String nombre, String apellido, String fdn, String password, String email) {
+    	
+		String newPassword = ph.hashPassword(password);
+		String[] obj = {cedula, nombre, apellido, fdn, newPassword, email};
+		String message = "";
+		
+		try {
+			boolean result = conn.psRegistro(connection, obj);
+			
+			if(result == true) {
+				message = "{\"message\": \"Registro Exitoso\", \"status\": 200 }";
+			}else {
+				message = "{\"message\": \"El Registro fue fallido\", \"status\": 503 }";
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return message;
+		
+		
+	}
+    
+    //Login
+    public String login(String cedula, String password) {
+    	String newPassword = ph.hashPassword(password);
+    	String[] obj = {cedula, password};
+		String message = "";
+    	
+		try {
+			boolean result = conn.psLogin(connection, obj);
+			
+			if(result == true) {
+				message = "{\"message\": \"Login Exitoso\", \"status\": 200 }";
+			}else {
+				message = "{\"message\": \"El Login fue fallido\", \"status\": 503 }";
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	
+    	
+    	
+    	return message;
+    }
+    
+    
+    
+    
+  
+    }
---
+++ src/helpers/Conexion.java
@@ -0,0 +1,110 @@
+package helpers;
+
+
+import java.sql.Connection;
+
+
+
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion {
+	
+	
+	PropertiesReader pr = new PropertiesReader();
+	private static Connection conn = null;
+	
+	
+	//Methods
+	public Connection getConnection() {
+		try {
+			if(conn == null) {
+				
+				String DBdriver = pr.getSQL("DBdriver");
+				String DBurl = pr.getSQL("DBurl");
+				String DBuser = pr.getSQL("DBuser");
+				String DBpassword = pr.getSQL("DBpassword");
+				
+			Class.forName(DBdriver);
+			conn = DriverManager.getConnection(DBurl, DBuser, DBpassword);
+			System.out.println("Conexion Exitosa");
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	//Register
+	public boolean psRegistro(Connection connection, String[] obj) {
+		
+		boolean result = false;
+		
+		try {
+			String sentence = pr.getSQL("registro");
+			
+			PreparedStatement ps = connection.prepareStatement(sentence);
+			ps = connection.prepareStatement(sentence);
+			
+			
+			ps.setString(1,  obj[0]);
+			ps.setString(2, obj[1]);
+			ps.setString(3, obj[2]);
+			ps.setString(4,  obj[3]);
+			ps.setString(5, obj[4]);
+			ps.setString(6, obj[5]);
+			ps.executeUpdate();
+			ps.close();
+			result = true;
+		
+		} catch (SQLException e) {
+			result = false;
+		}
+		
+		return result;
+		
+	}
+	
+	
+	//Login
+	public boolean psLogin(Connection connection, String[] obj) {
+		boolean result = false;
+		
+		try {
+			String sentence = pr.getSQL("login");
+			PreparedStatement ps;
+			ResultSet rs;
+			
+			ps = conn.prepareStatement(sentence);
+			ps.setString(1, obj[0]);
+			ps.setString(2, obj[1]);
+			
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				result = true;
+			}
+			
+		
+		} catch (SQLException e) {
+			result = false;
+		}
+		
+		
+		
+		return result;
+	}
+	
+	//Delete
+	
+	//Update
+	
+	
+	
+	
+	
+}
